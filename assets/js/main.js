32e03f73e0547b4c668d8bd8e8bcc9182584d394
eeegrr <evelinag8@gmail.com>
2025-09-14 01:45:23 +0300

    yes

--- assets/js/main.js
+++ assets/js/main.js
@@ -123,6 +123,22 @@ filtersEl.addEventListener('click', (e) => {
     });
 });
 
+/*=============== SEND MESSAGE ===============*/
+const form = document.querySelector('form');
+
+function sendEmail() {
+    Email.send({
+        Host: "s1.maildns.net",
+        Username: "username",
+        Password: "password",
+        To: 'them@website.com',
+        From: "you@isp.com",
+        Subject: "This is the subject",
+        Body: "And this is the body"
+    }).then(
+        message => alert(message)
+    );
+}
 
 sr.reveal('.home-img, .new-data, .care-img, .contact-content , .footer')
 sr.reveal('.home-data, .care-list, .contact-img', { delay: 500 })
